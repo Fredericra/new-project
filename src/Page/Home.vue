ef7fc302f785713f68cccfb24019bbfb6f082b37
Fredericra <randriatsilavinafrederic0@gmail.com>
2023-11-21 12:51:06 +0300

    package

---
+++ src/Page/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Right Bottom prompts info"
+        placement="right-end"
+      >
+        <el-button>right-end</el-button>
+      </el-tooltip>
+        <div class="text-blue-400 text-400">Hello <i class="fas fa-facebook.com fa-2x"></i></div>
+        <p class="text-gray-800 text-[32px]">hedlfdfldfkdl</p>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import './assets/main.css'
-
+import './assets/base.css'
+import 'element-plus/dist/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import AOS from 'aos';
+
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
+app.use(AOS.init())
 
 app.mount('#app')
